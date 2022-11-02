bbe002d5d9e049cd662b396376498368cb9f285a
Anthony Renard <63590543+RenardAnthony@users.noreply.github.com>
2022-11-02 18:55:36 +0100

    Add files via upload

---
+++ lang/ru.php
@@ -0,0 +1,222 @@
+<?php
+
+//Header
+$lang_about = "A propos";
+$lang_news = "Nouveauté";
+$lang_furIdentifier = "Fur Identifier";
+$lang_furMap = "Fur Map";
+$lang_login = "Connexion";
+$lang_recherchefurso = "Rechercher un fursona";
+$lang_nouserfound = "aucune utilisateur ne corresponde à votre recherche";
+
+
+
+//Footer
+$lang_cookie_titleUseCookie = "Ce site web utilise des cookies.";
+$lang_cookie_whantCookie = "Et vous, voulez-vous des cookies ?";
+$lang_condition = "Conditions d'utilisation";
+$lang_politique = "Politique de confidentialité";
+$lang_reserved = "© Furry-id.com base de donnée";
+$lang_version = "0.1.2";
+
+
+
+//Alpha
+$lang_alpha = "Alpha $lang_version";
+$lang_alpha_desc = "Le site que vous visiter est en alpha <br>
+Ne divulguez pas d'information personnelles <br>
+Utilisez un mot de passe unique";
+
+
+
+//General
+$lang_yes = "Oui";
+$lang_no = "Non";
+$lang_other = "Autre";
+$lang_Man = "Homme";
+$lang_Woman = "Femme";
+$lang_noBinary = "Non binaire";
+$lang_update = "Mise à jour";
+$lang_little = "Petit";
+$lang_middel = "Moyen";
+$lang_big = "Grand";
+
+
+
+//Language
+$lang_frensh = "Français";
+$lang_german = "Allemand";
+$lang_spanish = "Espagnol";
+$lang_english = "Anglais";
+$lang_italian = "Italien";
+$lang_portuguese = "Portugais";
+$lang_russian = "Russe";
+
+
+
+//Accueil
+$lang_bandeau_1 = "Bienvenu sur <em>Furry-id.com</em> ! <br><br>
+Grâce à ce site fini de ne plus retrouver un furry que vous avez croisé ! <br>
+Simplifiez-vous la vie dans le fandom grâce à votre IDcarte qui contient toutes les informations sur vous et vos fursona.";
+$lang_progress = 'Progression du site : "Après une grand pause je me remet au boulot"';
+$lang_news_title = "Les News !";
+$lang_event_title = "Les évènement à venir";
+$lang_last_register = "Dernier inscrit :";
+$lang_last_furso = "Dernier Fursona ajouter :";
+
+
+
+//Account_Edition
+$lang_changePassword = "Changer de mot de passe";
+$lang_fursuiter = "Fursuitter";
+$lang_photovideo = "Photographe / vidéaste";
+$lang_writer = "Écrivain";
+$lang_musician = "Musicien";
+$lang_drawing = "Dessinateur";
+$lang_fursuitMaker = "Fursuit Maker";
+$lang_roleplay = "Role play";
+$lang_extralink = "Lien exterieur";
+$lang_username = "Nom d'utilisateur";
+$lang_name = "Nom";
+$lang_lastname = "Prenom";
+$lang_age = "Âge";
+$lang_mailAdresse = "Adresse mail";
+$lang_url_avatar = "Avatar URL";
+$lang_contry = "Pays";
+$lang_furry_from = "Furry depuis";
+
+
+
+//Account
+$lang_ages = "ans";
+$lang_genre = "Genre";
+$lang_langue = "Langue";
+$lang_nbr_fursona = "Nombre de fursona";
+$lang_furry_id_card = "Fur id card";
+$lang_register_from = "Inscription le";
+$lang_update_from = "Mise à jours le";
+$lang_edit_profil = "Edit profil";
+$lang_badge = "Badges";
+$lang_myfursona = "Mon Fursona";
+$lang_myFursonas = "Mes Fursona";
+$lang_add_fursona = "Ajouter";
+$lang_starnewlife = "Fursona";
+
+
+
+//Error
+$lang_error = "Erreur";
+$lang_error_unauthorized = "Utilisateur non authentifié";
+$lang_error_desc1 = "Pour des raisons inconnues, vous êtes un utilisateur fantôme. <br> Vous ne pouvez accéder à cette page.";
+$lang_error_whatdoyoudo = "Comment tu à fait ?";
+$lang_error_desc2 = "Cette erreur n'est pour le moment pas encore implantée dans les sites internet. <br> cette erreur surviendra normalement lors d'un achat rater à cause d'un payement refuser.";
+$lang_error_denied = "Accès refusé";
+$lang_error_desc3 = "Cette page t'es interdit";
+$lang_error_notfound = "Page non trouvée";
+$lang_error_desc4 = "Habituelle... Si c'est un lien qui t’a mené jusqu’à cette page il est recommandé de prévenir les développeurs du site.";
+$lang_error_unprogramed = "Erreur non programer";
+$lang_error_desc5 = "L'erreur retourner n’a pas été prévue sur le site. C'est tout caser.";
+
+
+
+//Forget
+$lang_yourmail = "Votre adresse mail";
+$lang_newspassword = "Envoyer un nouveau mot de passe";
+
+
+
+//FurryCreation
+$lang_male = "Mâle";
+$lang_female = "Femelle";
+$lang_holder_space = "Espece (ex : Polar Fox)";
+$lang_url_fursona = "URL : Photo du fursona";
+$lang_url_refshit = "URL : Refshit";
+$lang_principal_color = "Couleurs principales";
+$lang_eye_color = "Couleurs des yeux";
+$lang_fourur_tile = "Motifs fourrure";
+$lang_desc_autre = "Description autre";
+$lang_desc_furso = "Je porte toujours un colier rouge et un lance roquette.";
+$lang_mystory = "Voici mon histoire...";
+$lang_nosuit = "Pas de suite";
+$lang_partiel = "Partiel";
+$lang_full = "Full";
+$lang_quadsuit = "Quad suite";
+$lang_url_fursuit = "URL : Photo fursuit";
+$lang_url_touchzone = "URL : Touche zone";
+$lang_fursuit_padding = "Padding";
+$lang_fursuit_fursuitstyle = "Style de suite";
+$lang_fursuit_plantigrade = "Plantigrade";
+$lang_fursuit_digidrade = "Digidrade";
+$lang_fursuit_toony = "Toony";
+$lang_fursuit_realist = "Realist";
+$lang_fursuit_jaw = "Machoire";
+$lang_fursuit_horn = "Cornes / bois";
+$lang_fursuit_fix = "Fix";
+$lang_fursuit_amovible = "Amovible";
+$lang_fursuit_eye_type = "Plant";
+$lang_fursuit_eye_followMe = "FollowMe";
+$lang_fursuit_size = "Taille en metre ( ex : 1.82 )";
+$lang_fursuit_hug_no = "No hug";
+$lang_fursuit_hug_yes = "Ok hug";
+$lang_fursuit_speakinsuit_no = "Je ne parle pas en fursuit";
+$lang_fursuit_speakinsuit_yes= "Je parle en fursuit";
+$lang_fursuit_editfursona = "Editer ce fursona";
+$lang_fursuit_creationfursona = "Crée mon fursona";
+$lang_fursuit_tooth = "Dents";
+$lang_fursuit_nbr_finger = "Nombre de doigt";
+$lang_fursuit_tail = "Queue";
+$lang_fursuit_eye = "Yeux";
+$lang_fursuit_wing = "Ailes";
+
+
+
+//FurCard
+$lang_edit_fursona = "Edit fursona";
+$lang_belongs = "Appartien à ";
+$lang_fursona_story = "Histoire du fursona";
+$lang_space = "Espece";
+$lang_fursuit = "Fursuite";
+$lang_color = "Couleurs";
+$lang_eye_color = "Couleur des Yeux";
+$lang_stain = "Taches";
+$lang_description = "Description";
+$lang_convention_fait = "Convention participation";
+$lang_size = "Taille";
+$lang_type = "Type";
+$lang_horn = "Cornes";
+$lang_style = "Style";
+$lang_canSpeak = "Can speak";
+$lang_hug = "Calin";
+$lang_touchzone = "Touch zone";
+$lang_accesoir = "Accessoire(s)";
+$lang_fursuitmaker = "Fursuite Maker";
+
+
+
+//Login
+$lang_index_desc = "Besoin de vous retrouver, devenez qui vous voulez...";
+$lang_index_soustitre = "*Pour participer à la beta, rendez-vous sur le discord";
+$lang_login_retour = "Bon retour parmi nous !";
+$lang_nom_or_password = "Pseudo ou email";
+$lang_password = "Mot de passe";
+$lang_rememberme = "Se souvenir de moi";
+$lang_password_forget = "mot de passe oublier";
+$lang_connect = "Se connecter";
+$lang_notregister= "Pas encore inscrit ? c'est gratuit !";
+
+
+
+//Register
+$lang_registeryou = "Inscrit toi, c'est gratuit !";
+$lang_username_desc = "Nom d'utilisateur";
+$lang_mailadresse_desc = "Adresse email";
+$lang_confirmpassword = "Confirmation du mots de passe";
+$lang_conditiongeneralaception = "J'accept les condition général d'utilisation et les Privacy Policy";
+$lang_creatacount = "Crée mon compte";
+$lang_desjainscrit = "Déjà inscrit ? c'est par ici !";
+
+
+
+
+?>
+
